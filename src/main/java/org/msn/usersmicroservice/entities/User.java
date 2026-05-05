49d4b8f575714b157ef57a26fb518eaf77022859
MalicknND <ndiayemalick062@gmail.com>
2026-05-05 09:53:56 +0200

    🚀 feat: update user-role relationship to use PERSIST and MERGE cascade types

--- src/main/java/org/msn/usersmicroservice/entities/User.java
+++ src/main/java/org/msn/usersmicroservice/entities/User.java
@@ -23,7 +23,7 @@ public class User {
     private String email;
 
 //    un user peut avoir plusieurs roles et un role peut etre attribue a plusieurs users
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
 // la table de jointure entre user et role s'appelle user_role, elle contient une colonne user_id qui reference l'id de user et une colonne role_id qui reference l'id de role
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles;
